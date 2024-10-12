aadcec752aab983fb05856c78b4697e962448a1e
Fuqian Huang <hfq_zyx@163.com>
2024-10-12 12:20:27 +0800

    up

--- nvim/lua/yellowpay/plugins/colorscheme.lua
+++ nvim/lua/yellowpay/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "tiagovla/tokyodark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme tokyodark")
-    end,
+  "olimorris/onedarkpro.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme onedark")
+  end,
 }
